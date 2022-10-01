65477ac873df0bcf1a7c6a59fa4e7e2ab5689446
vaudois <info@vaudois.ch>
2022-10-02 00:20:14 +0200

    Update packagecompil.sh

--- utils/packagecompil.sh
+++ utils/packagecompil.sh
@@ -29,11 +29,8 @@ function package_compile_crypto
 	libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev zlib1g-dev libz-dev \
 	libseccomp-dev libcap-dev libminiupnpc-dev gettext libminiupnpc10 libcanberra-gtk-module libqrencode-dev libzmq3-dev \
 	libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-	hide_output sudo add-apt-repository -y ppa:bitcoin/bitcoin
 	hide_output sudo apt -y update && sudo apt -y upgrade
-	hide_output sudo apt -y install libdb4.8-dev libdb4.8++-dev libdb5.3 libdb5.3++
 
-	hide_output sudo apt-get -y update
 	hide_output sudo apt -y install	libgmp-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev \
 	libpgm-dev libhidapi-dev libusb-1.0-0-dev libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev \
 	libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev \
