8611720cb85d30b5e08d5fb15ee08e2a867d9e12
vaudois <info@vaudois.ch>
2022-10-02 18:47:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -254,7 +254,7 @@ clear
 		apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli
 		apt_install php7.3-cgi php-pear imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt
 		apt_install php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php-gettext php7.3-zip php7.3-mbstring
-		apt_install libpsl-dev libnghttp2-dev php7.3-memcache php7.3-memcached
+		apt_install libpsl-dev libnghttp2-dev php7.3-memcache php7.3-memcached php-curl
 	fi
 
 	sleep 5
