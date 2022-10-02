de96f8375f4ca92e56c33deb110e33e79b46910c
vaudois <info@vaudois.ch>
2022-10-02 13:50:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,17 +39,17 @@ clear
 	
 	sudo sed -i 's#btcdons#'$BTCDEP'#' conf/functions.sh
 	sleep 1
-	sudo sed -i 's#bchdons#'$BCHDEP'#' utils/addport.sh
+	sudo sed -i 's#bchdons#'$BTCDEP'#' utils/addport.sh
 	sleep 1
 
 	sudo sed -i 's#ltcdons#'$LTCDEP'#' conf/functions.sh
 	sleep 1
-	sudo sed -i 's#bchdons#'$BCHDEP'#' utils/addport.sh
+	sudo sed -i 's#ltcdons#'$LTCDEP'#' utils/addport.sh
 	sleep 1
 
 	sudo sed -i 's#ethdons#'$ETHDEP'#' conf/functions.sh
 	sleep 1
-	sudo sed -i 's#bchdons#'$BCHDEP'#' utils/addport.sh
+	sudo sed -i 's#ethdons#'$ETHDEP'#' utils/addport.sh
 	sleep 1
 
 	sudo sed -i 's#bchdons#'$BCHDEP'#' conf/functions.sh
