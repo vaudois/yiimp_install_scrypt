b0f8ef611a560538ff05aac594cbd0446fe1fbf1
vaudois <info@vaudois.ch>
2022-12-15 22:50:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@
 #
 # Program:
 #   Install yiimp on Ubuntu 16.04/18.04 running Nginx, MariaDB, and php7.3
-#   v0.4
+#   v0.5
 # 
 ################################################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.4
+	TAG=v0.5
 fi
 
 NPROC=$(nproc)
