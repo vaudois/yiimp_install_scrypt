de6ee90bfe78d9f15056e2b1ef51c1106427e3fb
vaudois <info@vaudois.ch>
2022-12-16 03:43:55 +0100

    Update packagecompil.sh

--- utils/packagecompil.sh
+++ utils/packagecompil.sh
@@ -17,16 +17,21 @@ function package_compile_crypto
 
     hide_output sudo apt -y install software-properties-common build-essential
     hide_output sudo apt -y install libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev zlib1g-dev libz-dev libseccomp-dev libcap-dev libminiupnpc-dev gettext
-    hide_output sudo apt -y install libminiupnpc10 libzmq5
+    hide_output sudo apt -y install libzmq5
+	if [[ ("${DISTRO}" == "16" || "${DISTRO}" == "18") ]]; then
+		apt_get_quiet_time libminiupnpc10
+		sudo add-apt-repository -y ppa:bitcoin/bitcoin -qq > /dev/null 2>&1
+		sudo apt -y update && sudo apt -y upgrade -qq > /dev/null 2>&1
+		hide_output sudo apt -y install libdb4.8-dev libdb4.8++-dev libdb5.3 libdb5.3++
+	else
+		hide_output sudo apt -y install libminiupnpc-dev libdb5.3 libdb5.3++
+	fi
     hide_output sudo apt -y install libcanberra-gtk-module libqrencode-dev libzmq3-dev
     hide_output sudo apt -y install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-    hide_output sudo add-apt-repository -y ppa:bitcoin/bitcoin
-    hide_output sudo apt -y update
-    hide_output sudo apt -y install libdb4.8-dev libdb4.8++-dev libdb5.3 libdb5.3++
 
 	hide_output sudo apt-get -y install build-essential libzmq5 \
 	libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev zlib1g-dev libz-dev \
-	libseccomp-dev libcap-dev libminiupnpc-dev gettext libminiupnpc10 libcanberra-gtk-module libqrencode-dev libzmq3-dev \
+	libseccomp-dev libcap-dev libminiupnpc-dev gettext libcanberra-gtk-module libqrencode-dev libzmq3-dev \
 	libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
 	hide_output sudo apt -y update && sudo apt -y upgrade
 
