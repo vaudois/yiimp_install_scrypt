9a68f580998b7b5953e62188a511162de20cbc1d
vaudois <info@vaudois.ch>
2022-12-16 01:13:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -103,9 +103,9 @@ clear
 	echo -e "$CYAN => Updating system and installing required packages :$COL_RESET"
 	sleep 3
         
-	sudo apt -y update 
-	sudo apt -y upgrade
-	sudo apt -y autoremove
+	hide_output sudo apt -y update 
+	hide_output sudo apt -y upgrade
+	hide_output sudo apt -y autoremove
 	apt_install dialog python3 python3-pip acl nano apt-transport-https
 	apt_install figlet
 	echo -e "$GREEN Done...$COL_RESET"
