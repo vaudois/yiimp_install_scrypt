41c1ce839ae03e669d570c9ed784840e7c9b749d
vaudois <info@vaudois.ch>
2022-12-18 01:44:45 +0100

    Update screen-scrypt.sh

--- conf/screen-scrypt.sh
+++ conf/screen-scrypt.sh
@@ -4,9 +4,9 @@
  STRATUM_DIR=/var/stratum
  USR_BIN=/usr/bin
  
- screen -dmS main bash $WEB_DIR/main.sh
- screen -dmS loop2 bash $WEB_DIR/loop2.sh
- screen -dmS blocks bash $WEB_DIR/blocks.sh
+ screen -dmS main bash $WEB_DIR/crons/main.sh
+ screen -dmS loop2 bash $WEB_DIR/crons/loop2.sh
+ screen -dmS blocks bash $WEB_DIR/crons/blocks.sh
  screen -dmS debug tail -f $LOG_DIR/debug.log
  
 
