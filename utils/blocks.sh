366d80032d110b33ae65b3392457b38a747a4ff6
vaudois <info@vaudois.ch>
2022-12-18 06:04:55 +0100

    Update blocks.sh

--- utils/blocks.sh
+++ utils/blocks.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+source /etc/serveryiimp.conf
+
 PHP_CLI='php -d max_execution_time=60'
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${DIR}
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#cd ${DIR}
+
+cd ${STORAGE_SITE}/
 
 date
 echo started in ${DIR}
@@ -13,5 +17,3 @@ while true; do
         sleep 20
 done
 exec bash
-
-
