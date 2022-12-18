14874b1849655089acd77521c01f6de4f263a23d
vaudois <info@vaudois.ch>
2022-12-18 06:06:56 +0100

    Update main.sh

--- utils/main.sh
+++ utils/main.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+source /etc/serveryiimp.conf
+
 PHP_CLI='php -d max_execution_time=120'
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${DIR}
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#cd ${DIR}
+
+cd ${STORAGE_SITE}/
 
 date
 echo started in ${DIR}
@@ -13,4 +17,3 @@ while true; do
         sleep 90
 done
 exec bash
-
