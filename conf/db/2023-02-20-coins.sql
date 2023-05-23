a7647a59ef2df494498dd156451bc6c59ecba205
vaudois <info@vaudois.ch>
2023-05-23 22:43:45 +0200

    Update 2023-02-20-coins.sql

--- conf/db/2023-02-20-coins.sql
+++ conf/db/2023-02-20-coins.sql
@@ -3,5 +3,6 @@
 
 -- don't forget to restart memcached service to refresh the db structure
 ALTER TABLE `coins` ADD `tags` VARCHAR(50) NULL AFTER `watch`;
-
+ALTER TABLE `coins` ADD `test_coin` VARCHAR(50) NULL AFTER `auxpow`;
+ALTER TABLE `coins` ADD `usemweb` VARCHAR(50) NULL AFTER `usesegwit`;
 ALTER TABLE `coins` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1300;
