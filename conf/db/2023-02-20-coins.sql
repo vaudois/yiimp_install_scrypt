d9c974947d557d4a9589c9e44d2247e272c5af2a
vaudois <info@vaudois.ch>
2023-05-23 22:48:49 +0200

    Update 2023-02-20-coins.sql

--- conf/db/2023-02-20-coins.sql
+++ conf/db/2023-02-20-coins.sql
@@ -2,6 +2,7 @@
 -- mysql yaamp -p < file.sql
 
 -- don't forget to restart memcached service to refresh the db structure
+
 ALTER TABLE `coins` ADD `tags` VARCHAR(50) NULL AFTER `watch`;
 ALTER TABLE `coins` ADD `test_coin` VARCHAR(50) NULL AFTER `auxpow`;
 ALTER TABLE `coins` ADD `usemweb` VARCHAR(50) NULL AFTER `usesegwit`;
