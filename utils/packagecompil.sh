e90ff7739ec36e33c54a8021679d6d31e30f896a
vaudois <info@vaudois.ch>
2023-07-03 01:18:47 +0200

    Update packagecompil.sh

--- utils/packagecompil.sh
+++ utils/packagecompil.sh
@@ -10,18 +10,17 @@
 function package_compile_crypto
 {
     # Installing Package to compile crypto currency
-    output " "
-    output "Installing needed Package to compile crypto currency"
-    output " "
+    output " >--> Installing needed Package to compile crypto currency"
     sleep 3
 
     	apt_install libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev zlib1g-dev libz-dev
 	apt_install libzmq5 libseccomp-dev libcap-dev libminiupnpc-dev gettext
 	if [ "${DISTRO}" == "16" ] || [ "${DISTRO}" == "18" ]; then
-
 		apt_install libminiupnpc10
 		sudo add-apt-repository -y ppa:bitcoin/bitcoin -qq > /dev/null 2>&1
+  		output " >--> Updating & refresh system on Distro ${DISTRO}..."
 		sudo apt -y update && sudo apt -y upgrade -qq > /dev/null 2>&1
+  		sleep 2
 		apt_install libdb4.8-dev libdb4.8++-dev libdb5.3 libdb5.3++
 	else
 		apt_install libminiupnpc-dev libdb5.3 libdb5.3++
@@ -31,6 +30,7 @@ function package_compile_crypto
 	apt_install build-essential libzmq5 libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev
 	apt_install zlib1g-dev libz-dev libseccomp-dev libcap-dev libminiupnpc-dev gettext libcanberra-gtk-module libqrencode-dev libzmq3-dev
 	apt_install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+ 	output " >--> Updating system Distro ${DISTRO}..."
 	hide_output sudo apt -y update && sudo apt -y upgrade > /dev/null 2>&1
 
 	apt_install install libgmp-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev
