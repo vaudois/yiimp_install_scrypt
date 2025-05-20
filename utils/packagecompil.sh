56645b7afec8b201db2277ed6ccdf1180ee9f58a
vaudois <info@vaudois.ch>
2025-05-20 02:22:51 +0200

    Update packagecompil.sh

--- utils/packagecompil.sh
+++ utils/packagecompil.sh
@@ -3,7 +3,7 @@
 #
 # Program:
 #   Install needed packages to compile cryptocurrency on Ubuntu 20.04/22.04
-# 
+#
 ################################################################################
 
 function package_compile_crypto
@@ -11,9 +11,15 @@ function package_compile_crypto
     echo " >--> Installing needed packages to compile cryptocurrency"
     sleep 3
 
+    # Mise à jour du système
+    echo " >--> Updating system for Ubuntu 20.04/22.04..."
+    sudo apt -y update && sudo apt -y upgrade -qq > /dev/null 2>&1
+    sudo apt -y autoremove -qq > /dev/null 2>&1
+
     # Paquets de base pour la compilation
     apt_install build-essential libtool gettext bsdmainutils git cmake autotools-dev automake pkg-config libzmq3-dev
     apt_install libssl-dev libevent-dev libseccomp-dev libcap-dev libminiupnpc-dev libboost-all-dev zlib1g-dev
+    apt_install libgmp-dev libmariadb-dev libkrb5-dev gnutls-dev screen
 
     # Berkeley DB pour la compatibilité avec les portefeuilles de cryptomonnaies
     apt_install libdb5.3-dev libdb5.3++-dev
@@ -23,18 +29,23 @@ function package_compile_crypto
     apt_install libcanberra-gtk-module libqrencode-dev
 
     # Dépendances réseau et autres
-    apt_install libgmp-dev libunbound-dev libsodium-dev libunwind-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev
+    apt_install libunbound-dev libsodium-dev libunwind-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev
     apt_install libpgm-dev libhidapi-dev libusb-1.0-0-dev libudev-dev
     apt_install libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev
     apt_install libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev
     apt_install libboost-system-dev libboost-thread-dev
-    apt_install default-libmysqlclient-dev librtmp-dev libssh2-1-dev libldap2-dev libidn11-dev
+    apt_install default-libmysqlclient-dev librtmp-dev libssh2-1-dev libldap2-dev
     apt_install libbrotli-dev libssh-dev libnghttp2-dev libpsl-dev
     apt_install python3 ccache doxygen graphviz
 
-    # Mise à jour du système
-    echo " >--> Updating system for Ubuntu 20.04/22.04..."
-    sudo apt -y update && sudo apt -y upgrade -qq > /dev/null 2>&1
+    # Paquets spécifiques à Ubuntu 20.04 ou 22.04
+    if [[ "$DISTRO" == "20" ]]; then
+        apt_install libcurl4-gnutls-dev libidn11-dev
+    elif [[ "$DISTRO" == "22" ]]; then
+        apt_install libcurl4-openssl-dev libidn2-dev
+    fi
+
+    echo -e "$GREEN Done...$COL_RESET"
 }
 
 # Fonction utilitaire pour installer les paquets
