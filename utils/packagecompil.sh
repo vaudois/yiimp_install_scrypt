eecb11845a5ab820076fed02d61b1d3989903d51
vaudois <info@vaudois.ch>
2025-05-20 02:15:45 +0200

    Update packagecompil.sh

--- utils/packagecompil.sh
+++ utils/packagecompil.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
-################################################################################ 
+################################################################################
 #
 # Program:
-#   Install needed Package to compile crypto currency
+#   Install needed packages to compile cryptocurrency on Ubuntu 20.04/22.04
 # 
 ################################################################################
-    
-	
+
 function package_compile_crypto
 {
-    # Installing Package to compile crypto currency
-    output " >--> Installing needed Package to compile crypto currency"
+    echo " >--> Installing needed packages to compile cryptocurrency"
     sleep 3
 
-    	apt_install build-essential libtool gettext bsdmainutils git cmake autotools-dev automake pkg-config libzmq5
-	apt_install libssl-dev libevent-dev libseccomp-dev libcap-dev libminiupnpc-dev libboost-all-dev zlib1g-dev libz-dev
-	if [ "${DISTRO}" == "16" ] || [ "${DISTRO}" == "18" ]; then
-		apt_install libminiupnpc10
-		sudo add-apt-repository -y ppa:bitcoin/bitcoin -qq > /dev/null 2>&1
-  		output " >--> Updating & refresh system on Distro ${DISTRO}..."
-		sudo apt -y update && sudo apt -y upgrade -qq > /dev/null 2>&1
-  		sleep 2
-		apt_install libdb4.8-dev libdb4.8++-dev libdb5.3 libdb5.3++
-	else
-		apt_install libminiupnpc-dev libdb5.3 libdb5.3++
-	fi
-    	apt_install libcanberra-gtk-module libqrencode-dev libzmq3-dev
-    	apt_install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-	apt_install zlib1g-dev libz-dev libseccomp-dev libcap-dev libminiupnpc-dev gettext libcanberra-gtk-module libqrencode-dev libzmq3-dev
-	apt_install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
- 	output " >--> Updating system Distro ${DISTRO}..."
-	hide_output sudo apt -y update && sudo apt -y upgrade > /dev/null 2>&1
+    # Paquets de base pour la compilation
+    apt_install build-essential libtool gettext bsdmainutils git cmake autotools-dev automake pkg-config libzmq3-dev
+    apt_install libssl-dev libevent-dev libseccomp-dev libcap-dev libminiupnpc-dev libboost-all-dev zlib1g-dev
 
-	apt_install install libgmp-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev
-	apt_install libpgm-dev libhidapi-dev libusb-1.0-0-dev libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev
-	apt_install libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev
-	apt_install default-libmysqlclient-dev librtmp-dev libssh2-1 libssh2-1-dev libldap2-dev libidn11-dev
- 	apt_install liblbfgs-dev liblbfgsb-dev libbrotli-dev libssh-dev libnghttp2-dev libpsl-dev
-  	apt_install python3 ccache doxygen graphviz
+    # Berkeley DB pour la compatibilité avec les portefeuilles de cryptomonnaies
+    apt_install libdb5.3-dev libdb5.3++-dev
 
+    # Paquets pour l'interface graphique (Qt)
+    apt_install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+    apt_install libcanberra-gtk-module libqrencode-dev
+
+    # Dépendances réseau et autres
+    apt_install libgmp-dev libunbound-dev libsodium-dev libunwind-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev
+    apt_install libpgm-dev libhidapi-dev libusb-1.0-0-dev libudev-dev
+    apt_install libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev
+    apt_install libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev
+    apt_install libboost-system-dev libboost-thread-dev
+    apt_install default-libmysqlclient-dev librtmp-dev libssh2-1-dev libldap2-dev libidn11-dev
+    apt_install libbrotli-dev libssh-dev libnghttp2-dev libpsl-dev
+    apt_install python3 ccache doxygen graphviz
+
+    # Mise à jour du système
+    echo " >--> Updating system for Ubuntu 20.04/22.04..."
+    sudo apt -y update && sudo apt -y upgrade -qq > /dev/null 2>&1
 }
+
+# Fonction utilitaire pour installer les paquets
+function apt_install
+{
+    sudo apt install -y "$@"
+}
+
+# Appel de la fonction
+package_compile_crypto
