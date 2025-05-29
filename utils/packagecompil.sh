906a2e54740b7812b83f10abd2619994e197372b
vaudois <info@vaudois.ch>
2025-05-29 11:35:27 +0200

    Update packagecompil.sh

--- utils/packagecompil.sh
+++ utils/packagecompil.sh
@@ -21,13 +21,19 @@ function package_compile_crypto
     apt_install build-essential libc6-dev libgcc-11-dev libtool gettext bsdmainutils git cmake autotools-dev automake pkg-config libzmq3-dev
     apt_install libssl-dev libevent-dev libseccomp-dev libcap-dev libminiupnpc-dev libboost-all-dev zlib1g-dev
     apt_install libgmp-dev libmariadb-dev libkrb5-dev gnutls-dev screen
-
+	
+	clear
+	term_art_server
+	
     # Berkeley DB pour la compatibilité avec les portefeuilles de cryptomonnaies
     apt_install libdb5.3-dev libdb5.3++-dev
 
     # Paquets pour l'interface graphique (Qt)
     apt_install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
     apt_install libcanberra-gtk-module libqrencode-dev
+
+	clear
+	term_art_server
 
     # Dépendances réseau et autres
     apt_install libunbound-dev libsodium-dev libunwind-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev
@@ -39,12 +45,18 @@ function package_compile_crypto
     apt_install libbrotli-dev libssh-dev libnghttp2-dev libpsl-dev
     apt_install python3 ccache doxygen graphviz  # Retiré libmysqlclient-dev
 
+	clear
+	term_art_server
+
     # Paquets spécifiques à Ubuntu 20.04 ou 22.04
     if [[ "$DISTRO" == "20" ]]; then
         apt_install libcurl4-gnutls-dev libidn11-dev
     elif [[ "$DISTRO" == "22" ]]; then
         apt_install libcurl4-openssl-dev libidn2-dev
     fi
+
+	clear
+	term_art_server
 
     # Créer un lien symbolique pour mariadb_config -> mysql_config pour compatibilité avec les scripts de compilation
     if [ -f /usr/bin/mariadb_config ] && [ ! -f /usr/bin/mysql_config ]; then
